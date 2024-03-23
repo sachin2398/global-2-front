e6226a5ea874a396d3423c0bf75cae9426e691e5
sachin2398 <smartsachin38@gmail.com>
2024-03-23 22:45:20 +0530

    chek

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react'
-import { AuthContext } from '../Context/AuthContext'
+import React, { } from 'react'
+
 import SignupBox from './SignupBox'
 
 
 
 const Home = () => {
-  const { authState,logout} = useContext(AuthContext);
+
   return (
   <SignupBox/>
   
